996232c953ee2ab50d45859ff7b19fbfccbc8e35
evans <evan0284@algonquinlive.com>
2023-11-29 11:54:35 -0500

    Initial Commit

---
+++ mappp/mapppApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapppApp.swift
+//  mappp
+//
+//  Created by Evans on 2023-11-29.
+//
+
+import SwiftUI
+
+@main
+struct mapppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
