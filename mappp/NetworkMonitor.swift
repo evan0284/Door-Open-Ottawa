b77c2b38df3831ea8b00f2fc32d2d66faffae25b
evans <evan0284@algonquinlive.com>
2023-12-02 19:05:10 -0500

    recomended building added

---
+++ mappp/NetworkMonitor.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkMonitor.swift
+//  mappp
+//
+//  Created by Evans on 2023-12-02.
+//
+
+import Foundation
---
+++ mappp/RecomendedBuilding.swift
@@ -0,0 +1,18 @@
+//
+//  RecomendedBuilding.swift
+//  mappp
+//
+//  Created by Evans on 2023-12-02.
+//
+
+import SwiftUI
+
+struct RecomendedBuilding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecomendedBuilding()
+}
